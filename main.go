f68e25711029e68e7d58394297d5c1e1c3982793
Bogdan Popa <popa.bogdanp@gmail.com>
2016-09-15 12:01:11 +0300

    add cpuprofile param

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"log"
 	"net/url"
+	"os"
 	"runtime"
+	"runtime/pprof"
 
 	"github.com/Bogdanp/sitemapper/crawler"
 )
@@ -12,11 +14,13 @@ import (
 var start string
 var concurrency int
 var verbose bool
+var cpuprofile string
 
 func init() {
 	flag.IntVar(&concurrency, "concurrency", runtime.NumCPU(), "The maximum number of concurrent requests to run.")
 	flag.StringVar(&start, "start", "http://example.com", "The URL to start crawling.")
 	flag.BoolVar(&verbose, "verbose", false, "Print debug info.")
+	flag.StringVar(&cpuprofile, "cpuprofile", "", "Write CPU profile.")
 }
 
 func assert(err error, message string, params ...interface{}) {
@@ -27,6 +31,13 @@ func assert(err error, message string, params ...interface{}) {
 
 func main() {
 	flag.Parse()
+
+	if cpuprofile != "" {
+		f, err := os.Create(cpuprofile)
+		assert(err, "failed to create cpuprifle file: %v", err)
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	root, err := url.Parse(start)
 	assert(err, "%q is not a valid URL", start)
